b4961bfdb83449e9e6845282368413b23cab7289
dbbnicole <dbbnicole>
2022-08-06 03:43:45 +0000

    Add notebooks

---
+++ config/Data Extract.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# MAGIC %sh 
+# MAGIC cd /databricks/driver
+# MAGIC mkdir -p /dbfs/tmp/clv/online_retail
+# MAGIC wget -N http://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx -P /dbfs/tmp/clv/online_retail
+
+# COMMAND ----------
+
+
